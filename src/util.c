f16f0e446fd987dd61f0d3a99add510d8d9b5810
zlwgx <389179924@qq.com>
2017-01-19 21:23:49 +0800

    Add util

---
+++ src/util.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "util.h"
+
+void muse_log(const char *fmt, ...)
+{
+    FILE *log_file = fopen("log/muse.log", "a+");
+    if (!log_file)
+        return;
+
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    if (!tm)
+        return;
+
+    fprintf(log_file, "[%4d: %02d: %02d: %02d:%02d:%02d] [pid: %5d]",
+            tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
+            tm->tm_hour, tm->tm_min, tm->tm_sec, getpid());
+
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(log_file, fmt, ap);
+    va_end(ap);
+    fprintf(log_file, "\n");
+    fclose(log_file);
+}
