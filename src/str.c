6a2435260f4ea55e354b8728bf40d74f6a131837
zlwgx <389179924@qq.com>
2017-01-25 22:18:16 +0800

    Add str_eq

--- src/str.c
+++ src/str.c
@@ -8,3 +8,16 @@ void str_init(str_t *str)
     str->str = NULL;
     str->len = 0;
 }
+
+bool str_eq(const str_t *l, const str_t *r)
+{
+    assert(l && r);
+
+    if (l->len != r->len)
+        return false;
+    for (size_t i = 0; i < l->len; i++) {
+        if (l->str[i] != r->str[i])
+            return false;
+    }
+    return true;
+}
--- src/str.h
+++ src/str.h
@@ -1,6 +1,8 @@
 #ifndef MUSE_STR_H__
 #define MUSE_STR_H__
 
+#include <stdbool.h>
+
 typedef struct {
     char *str;
     size_t len;
@@ -9,5 +11,6 @@ typedef struct {
 #define STR(s) (str_t){s, sizeof(s) - 1}
 
 void str_init(str_t *str);
+bool str_eq(const str_t *l, const str_t *r);
 
 #endif
